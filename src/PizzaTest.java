c55e7acb8843128abb7ed830e31be0b8f74a9447
Douglas <douglasaugusto.mello@gamil.com>
2026-08-31 11:54:43 -0300

    incluindo teste para pizza

--- src/PizzaTest.java
+++ src/PizzaTest.java
@@ -1,20 +1,54 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PizzaTest {
+    Pizza pizza;
+    
+    @BeforeEach
+    public void setUp(){
+        //Arrange
+        pizza = new Pizza();
+        pizza.adicionarIngredientes(2);
+    }
 
     @Test
     public void adicionaIngredientesCorretamente(){
-        //Arrange
-        Pizza pizza = new Pizza();
-
         //Act
         int quantos = 
             pizza.adicionarIngredientes(4);
 
         //Assert
-        assertEquals(4, quantos);
+        assertEquals(6, quantos);
+    }
+    @Test
+    public void naoAdicionaIgredienteNegativo(){
+       
+        //Act
+        int quantidade = pizza.adicionarIngredientes(-67);
+
+        //Assert
+        assertEquals(2,quantidade);
+
+    }
+    @Test
+    public void naoUltrapassaMaxIngredientes(){
+         
+        //Act
+        int quantidade = pizza.adicionarIngredientes(67);
+
+        //Assert
+        assertEquals(2, quantidade);
+
+    }
+    @Test
+    public void calcularPrecoCorretamente(){
+      
+        //act
+        double preco = pizza.valorFinal();
+        //assert
+        assertEquals(39, preco,0.01);
     }
     
 }
